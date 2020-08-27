ccb7af8f5d96631870781e63f73e84cd09be6050
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-27 16:35:30 +0800

    [refactor-exercise][sam] refactor : refactor function

--- src/statement.js
+++ src/statement.js
@@ -1,3 +1,5 @@
+const COMEDY = 'comedy';
+const TRAGEDY = 'tragedy';
 getPlayFor = (plays, perf) => {
     return plays[perf.playID];
 };
@@ -10,43 +12,64 @@ buildFormatObj = () => {
     }).format;
 };
 
-buildTextResult = (plays, invoice, format) => {
+getVolumeCredits = (plays, invoice) => {
     let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        const play = getPlayFor(plays, perf);
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        if (COMEDY === play.type) {
+            volumeCredits += Math.floor(perf.audience / 5);
+        }
+    }
+    return volumeCredits;
+};
+
+getTotalAmount = (performances, plays) => {
     let totalAmount = 0;
-    let result = `Statement for ${invoice.customer}\n`;
+    for (let perf of performances) {
+        const play = getPlayFor(plays, perf);
+        let thisAmount = countThisAmount(play.type, perf);
+        totalAmount += thisAmount;
+    }
+    return totalAmount;
+};
+
+getResultBodyDetail = (invoice, plays, format) => {
+    let result;
     for (let perf of invoice.performances) {
         const play = getPlayFor(plays, perf);
         let thisAmount = countThisAmount(play.type, perf);
-        // add volume credits
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        // add extra credit for every ten comedy attendees
-        if ('comedy' === play.type) {
-            volumeCredits += Math.floor(perf.audience / 5);
-        }
-        //print line for this order
         result += ` ${play.name}: ${format(thisAmount / 100)} (${perf.audience} seats)\n`;
-        totalAmount += thisAmount;
     }
-    result += `Amount owed is ${format(totalAmount / 100)}\n`;
-    result += `You earned ${volumeCredits} credits \n`;
     return result;
 };
 
-function statement(invoice, plays) {
+getConnect = (detail, format, invoice, plays, volumeCredits) => {
+    return detail + `Amount owed is ${format(getTotalAmount(invoice.performances, plays) / 100)}\n` + `You earned ${volumeCredits} credits \n`;
+};
+
+buildTextResult = (plays, invoice, format) => {
+    let volumeCredits = 0;
+    let result = `Statement for ${invoice.customer}\n`;
+    result += getResultBodyDetail(invoice, plays, format);
+    return result + `Amount owed is ${format(getTotalAmount(invoice.performances, plays) / 100)}\n` + `You earned ${getVolumeCredits(plays, invoice)} credits \n`;
+};
+
+statement = (invoice, plays) => {
     const format = buildFormatObj();
     return buildTextResult(plays, invoice, format);
-}
+};
 
 countThisAmount = (playType, perf) => {
     let thisAmount = 0;
     switch (playType) {
-        case 'tragedy':
+        case TRAGEDY:
             thisAmount = 40000;
             if (perf.audience > 30) {
                 thisAmount += 1000 * (perf.audience - 30);
             }
             break;
-        case 'comedy':
+        case COMEDY:
             thisAmount = 30000;
             if (perf.audience > 20) {
                 thisAmount += 10000 + 500 * (perf.audience - 20);
