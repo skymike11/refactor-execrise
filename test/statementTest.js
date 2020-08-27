b5a343d38a98d7d15a09bd5add6debb62946420f
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-27 14:21:08 +0800

    [refactor-exercise][sam] refactor : format code

--- test/statementTest.js
+++ test/statementTest.js
@@ -2,54 +2,54 @@ const test = require('ava');
 const {statement} = require('../src/statement');
 
 test('Sample test', t => {
-  t.true(true);
-  t.is(1, 1);
-  t.deepEqual({a: 1}, {a: 1});
+    t.true(true);
+    t.is(1, 1);
+    t.deepEqual({a: 1}, {a: 1});
 });
 
-test('Sample test', t => {
-  //given
-  const invoice = {};
-  const plays = [];
+test('statement case 1. Customer BigCo has one performances hamlet and the audience is 30. ', t => {
+    //given
+    const invoice = {};
+    const plays = [];
 
-  // const result = statement(invoice, plays);
-  const result = '';
+    const result = statement(invoice, plays);
+    // const result = '';
 
 
-  t.is(result, '');
+    t.is(result, '');
 });
 
 
 const invoice = {
-  'customer': 'BigCo',
-  'performances': [
-    {
-      'playID': 'hamlet',
-      'audience': 55,
-    },
-    {
-      'playID': 'as-like',
-      'audience': 35,
-    },
-    {
-      'playID': 'othello',
-      'audience': 40,
-    },
-  ],
+    'customer': 'BigCo',
+    'performances': [
+        {
+            'playID': 'hamlet',
+            'audience': 55,
+        },
+        {
+            'playID': 'as-like',
+            'audience': 35,
+        },
+        {
+            'playID': 'othello',
+            'audience': 40,
+        },
+    ],
 };
 
 
 const plays = {
-  'hamlet': {
-    'name': 'Hamlet',
-    'type': 'tragedy',
-  },
-  'as-like': {
-    'name': 'As You Like It',
-    'type': 'comedy',
-  },
-  'othello': {
-    'name': 'Othello',
-    'type': 'tragedy',
-  },
+    'hamlet': {
+        'name': 'Hamlet',
+        'type': 'tragedy',
+    },
+    'as-like': {
+        'name': 'As You Like It',
+        'type': 'comedy',
+    },
+    'othello': {
+        'name': 'Othello',
+        'type': 'tragedy',
+    },
 };
