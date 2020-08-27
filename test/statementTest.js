905c994a6cb95cb611e21e023d1aff03339d4d80
OOCLDM\lisa8 <lisa8@oocl.com>
2020-08-27 13:51:55 +0800

    [refactor-exercise][sam] refactor : format code

--- test/statementTest.js
+++ test/statementTest.js
@@ -1,5 +1,55 @@
 const test = require('ava');
+const {statement} = require('../src/statement');
 
 test('Sample test', t => {
   t.true(true);
+  t.is(1, 1);
+  t.deepEqual({a: 1}, {a: 1});
 });
+
+test('Sample test', t => {
+  //given
+  const invoice = {};
+  const plays = [];
+
+  // const result = statement(invoice, plays);
+  const result = '';
+
+
+  t.is(result, '');
+});
+
+
+const invoice = {
+  'customer': 'BigCo',
+  'performances': [
+    {
+      'playID': 'hamlet',
+      'audience': 55,
+    },
+    {
+      'playID': 'as-like',
+      'audience': 35,
+    },
+    {
+      'playID': 'othello',
+      'audience': 40,
+    },
+  ],
+};
+
+
+const plays = {
+  'hamlet': {
+    'name': 'Hamlet',
+    'type': 'tragedy',
+  },
+  'as-like': {
+    'name': 'As You Like It',
+    'type': 'comedy',
+  },
+  'othello': {
+    'name': 'Othello',
+    'type': 'tragedy',
+  },
+};
